5831f8127bad3e0a8fa4b8082dd4bb69f111da8a
Valzionn <144949469+Valzionn@users.noreply.github.com>
2024-02-26 17:44:15 +0000

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,9 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      amber: colors.amber,
+      pink: colors.pink,
+      purple: colors.purple, 
       transparent: 'transparent',
       current: 'currentColor',
       black: colors.black,
@@ -23,10 +17,22 @@ module.exports = {
       emerald: colors.emerald,
       indigo: colors.indigo,
       yellow: colors.yellow,
+      blue: colors.blue,
+      violet: colors.violet
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
+      mono: ['ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;']
+    },
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      sxl: ['28px', '36px'],
+      ssxl: ['32px', '40px'],
+      sssxl: ['36px', '44px'],
     },
     extend: {
       spacing: {
