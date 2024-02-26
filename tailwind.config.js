dd0e4c1796b39da369353eef21d02599b8bb4b59
Valzionn <144949469+Valzionn@users.noreply.github.com>
2024-02-26 10:56:28 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      gray: {
+        500: '#34495e'
+      },
+      light: {
+        500: '#ecf0f1'
+      },
+      green: {
+        500: '##2ecc71'
+      },
+      orange: {
+        500: '#f39c12'
+      },
+      blue: {
+        500: '##3498db'
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
